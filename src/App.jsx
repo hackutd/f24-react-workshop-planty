e2b0c612ee3caa8b086e268d900e523f0aaf69e2
Michael Zhao <michaelzhao314@gmail.com>
2024-09-26 02:28:28 -0500

    Formatted all code with prettier and added prettierrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Card from './components/Card.jsx'
-import Navbar from './components/Navbar.jsx'
-import Menu from './components/Menu.jsx'
+import React from "react";
+import Card from "./components/Card.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Menu from "./components/Menu.jsx";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Menu />
       <Card />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,19 +16,23 @@ function Card() {
                   alt=""
                 />
               </div>
-              <button className="hover:text-text transition duration-150 w-24 mt-2 bg-backgroundDarker text-primary font-bold text-lg rounded-md">water</button>
+              <button className="hover:text-text transition duration-150 w-24 mt-2 bg-backgroundDarker text-primary font-bold text-lg rounded-md">
+                water
+              </button>
             </div>
             {/* Container for 2 divs, Name/Sellprice and Growth metric */}
             <div className="flex flex-col"></div>
             {/* Name and Sellprice */}
             <div className="flex flex-col mt-1 mr-24">
               <h1 className="text-3xl text-text font-bold ">Plant Name</h1>
-              <h1 className="text-textLight">Sell Price: <span className="text-primary text-lg">15g</span></h1>
-              <h1 className="m-auto text-text font-bold text-5xl">3<span className="text-textLight text-lg">/6 growth</span></h1>
+              <h1 className="text-textLight">
+                Sell Price: <span className="text-primary text-lg">15g</span>
+              </h1>
+              <h1 className="m-auto text-text font-bold text-5xl">
+                3<span className="text-textLight text-lg">/6 growth</span>
+              </h1>
             </div>
           </div>
-          
-
         </div>
       </div>
     </>
